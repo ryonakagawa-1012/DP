f0c6f49d668461a637b0b43eb52a687cd1710a4c
Ryo <ryo.na.na.ryo.na@gmail.com>
2024-04-08 11:55:58 +0900

    基本形の作成

---
+++ BaseModel.java
@@ -0,0 +1,9 @@
+public class BaseModel {
+    void run{
+        // 実行内容を書く
+    }
+    public static void main(String[] args){
+        BaseModel application = new BaseModel();
+        application.run;
+    }
+}
